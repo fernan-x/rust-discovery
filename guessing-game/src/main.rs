7bd38feaa602227ba73401bfce0ced1e2ff69973
Fabien Fernandes Alves <fwfabou@gmail.com>
2025-04-21 10:44:29 -0400

    feat: Compare number and quit when number found

--- guessing-game/src/main.rs
+++ guessing-game/src/main.rs
@@ -1,5 +1,6 @@
 use std::io;
 use rand::Rng;
+use std::cmp::Ordering;
 
 fn main() {
     println!("Guess the number!");
@@ -8,13 +9,26 @@ fn main() {
 
     println!("The secret number is: {secret_number}");
 
-    println!("Please input your guess.");
-
-    let mut guess = String::new();
-
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
-
-    println!("You guessed: {guess}");
+    loop {
+        println!("Please input your guess.");
+    
+        let mut guess = String::new();
+    
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+    
+        let guess: u8 = guess.trim().parse().expect("Please type a number!");
+    
+        println!("You guessed: {guess}");
+    
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
 }
