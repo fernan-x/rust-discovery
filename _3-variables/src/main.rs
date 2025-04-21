767c78fce0cb1ca787930ce0681a6147e0350995
Fabien Fernandes Alves <fwfabou@gmail.com>
2025-04-21 11:40:15 -0400

    feat: Start the function chapter

--- _3-variables/src/main.rs
+++ _3-variables/src/main.rs
@@ -1,3 +1,14 @@
+const DEFAULT_X_VALUE: u32 = 5;
+
 fn main() {
-    println!("Hello, world!");
+    let x = DEFAULT_X_VALUE;
+
+    let x = x + 1;
+
+    {
+        let x = x * 2;
+        println!("The value of x in the inner scope is: {x}");
+    }
+
+    println!("The value of x is: {x}");
 }
